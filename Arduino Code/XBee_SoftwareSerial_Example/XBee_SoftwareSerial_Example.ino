67c65d6fab8470f67f1a8e7326e0d1c8f02c208c
nofriendsinc <31297974+nofriendsinc@users.noreply.github.com>
2018-02-20 13:57:19 -0500

    Update

--- Arduino Code/XBee_SoftwareSerial_Example/XBee_SoftwareSerial_Example.ino
+++ Arduino Code/XBee_SoftwareSerial_Example/XBee_SoftwareSerial_Example.ino
@@ -31,7 +31,7 @@ SoftwareSerial mySerial(10, 11); // RX, TX
 
 void setup() {
   // Open serial communications and wait for port to open:
-  Serial.begin(57600);
+  Serial.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
@@ -40,7 +40,7 @@ void setup() {
   Serial.println("Goodnight moon!");
 
   // set the data rate for the SoftwareSerial port
-  mySerial.begin(4800);
+  mySerial.begin(9500);
   mySerial.println("Hello, world?");
 }
 
@@ -51,5 +51,6 @@ void loop() { // run over and over
   if (Serial.available()) {
     mySerial.write(Serial.read());
   }
-}
 
+  Serial.println("Goodnight moon!");
+}
